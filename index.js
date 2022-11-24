541c91904ea1a15d50a27735702e5f49625cbc7a
NabilNewaz <nabilnewaz@gmail.com>
2022-11-24 20:45:57 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
@@ -12,24 +13,37 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zusdhlj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run() {
     try {
         const categoriesCollection = client.db('eKapor').collection('categories');
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        })
+
         app.get('/categories', async (req, res) => {
-            const category_id = req.query.category_id;
-            if (category_id) {
-                const query = { _id: ObjectId(category_id) };
-                const cursor = categoriesCollection.find(query);
-                const category = await cursor.toArray();
-                res.send(category);
-            }
-            else {
-                const query = {};
-                const cursor = categoriesCollection.find(query);
-                const categories = await cursor.toArray();
-                res.send(categories);
-            }
+            const query = {};
+            const cursor = categoriesCollection.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+
         });
     }
     finally {
